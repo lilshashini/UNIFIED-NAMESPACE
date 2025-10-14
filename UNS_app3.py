b97b4fa879949c572113b3972cf1d044e88a0282
Shashini Sathsarani Laksiri <161680013+lilshashini@users.noreply.github.com>
2025-10-14 05:33:42 +0530

    Update UNS_app3.py

--- UNS_app3.py
+++ UNS_app3.py
@@ -2809,7 +2809,7 @@ def main():
     st.title("🏭 Factory Intelligence Agent")
     col1, col2 = st.columns([1, 16])
     with col1:
-        st.image("logo.png", width=300)
+        st.image("logo.png", width=350)
     with col2:
         st.markdown(
             "<h2 style='font-size:23px; transform: translateY(-18px); margin:0;'> Factory Copilot</h2>",
