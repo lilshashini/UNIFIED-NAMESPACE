66e9968e0ae567eba3b8e0aa9a9593c38f439171
Shashini Sathsarani Laksiri <161680013+lilshashini@users.noreply.github.com>
2025-10-14 05:45:45 +0530

    Update UNS_app3.py

--- UNS_app3.py
+++ UNS_app3.py
@@ -2812,7 +2812,7 @@ def main():
         st.image("logo.png", width=120)
     with col2:
         st.markdown(
-            "<h2 style='font-size:23px; transform: translateY(-10px) translateX(-18px); margin:0;'> Factory Copilot</h2>",
+            "<h2 style='font-size:23px; transform: translateY(-10px) translateX(-22px); margin:0;'> Factory Copilot</h2>",
             unsafe_allow_html=True
             )
     
