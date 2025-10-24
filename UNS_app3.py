616f8ec717e81a0f7272f23410987a0d1f808c5c
Shashini Sathsarani Laksiri <161680013+lilshashini@users.noreply.github.com>
2025-10-24 11:19:05 +0530

    Update logo and adjust title styling in app jay jay mills logo update

--- UNS_app3.py
+++ UNS_app3.py
@@ -2809,10 +2809,10 @@ def main():
     st.title("🏭 Factory Intelligence Agent")
     col1, col2 = st.columns([2, 20])
     with col1:
-        st.image("teejay_logo.png", width=120)
+        st.image("Jay_jay_mills_logo.png", width=200)
     with col2:
         st.markdown(
-            "<h2 style='font-size:23px; transform: translateY(-10px) translateX(-23px); margin:0;'> Factory Copilot</h2>",
+            "<h2 style='font-size:23px; transform: translateY(-20px) translateX(-12px); margin:0;'> Factory Copilot</h2>",
             unsafe_allow_html=True
             )
     
